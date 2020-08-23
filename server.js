91a5dc7ebdaff876357578f0c7962773ddf8304c
Sreyas Pillai <sreyasmpillai24@gmail.com>
2020-08-23 21:52:02 +0530

    commit

--- server.js
+++ server.js
@@ -2,17 +2,52 @@
 
 const express = require("express");
 
-
  const app = express();
 
-app.get("/",function(req,res){
-    res.send("hello");
+
+ function roman_conv(num){
+
+    var romanToNum ={
+     M:1000,
+     CM:900, 
+     D:500,
+     CD:400,
+     C:100,
+     XC:90,
+     L:50,
+     XL:40,
+     X:10,
+     IX:9,
+     V:5,
+     IV:4,
+     I:1
+    };
+
+    var roman = "";
+
+    for( var key in romanToNum ){
+        while(num >= romanToNum[key]){
+            roman += key;
+            num -= romanToNum[key];
+            
+        }
+    }
+
+    return(roman);
+
+};
+
+ 
+
+app.get("/roman_conv/:num",function(req,res){
+    var num = req.params.num;
+    res.send(roman_conv(num));
 });
 
 app.listen(3000, function(){
     console.log("Server started")
 
-})
+});
 
 
 
