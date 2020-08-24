bb3afde157aa7bc296d57336aa9a9f67bdfc32f8
Sreyas Pillai <sreyasmpillai24@gmail.com>
2020-08-24 08:54:17 +0530

    crud

---
+++ CRUD-exp/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.send('Hello GET');
+})
+
+app.post('/', function (req, res) {
+   console.log("Got a POST request for the homepage");
+   res.send('Hello POST');
+})
+
+app.delete('/del_user', function (req, res) {
+   console.log("Got a DELETE request for /del_user");
+   res.send('Hello DELETE');
+})
+
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("app listening at http://%s:%s", host, port)
+})
